e0c30a973a41d31c187b5dfa9f618f45dc86638b
springerw <springerw>
2007-01-14 20:52:29 +0000

    Added .cvsignore, support for interceptors, fixed a problem with the Java code getting generated.

--- blammo-maven-plugin/src/main/java/com/agilejava/blammo/mojo/BlammoGeneratorMojo.java
+++ blammo-maven-plugin/src/main/java/com/agilejava/blammo/mojo/BlammoGeneratorMojo.java
@@ -24,8 +24,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.antlr.stringtemplate.StringTemplate;
 import org.antlr.stringtemplate.StringTemplateGroup;
@@ -51,113 +53,131 @@ import com.thoughtworks.qdox.model.JavaMethod;
  */
 public class BlammoGeneratorMojo extends AbstractMojo {
 
-	/**
-	 * Include the message identifier.
-	 * 
-	 * @parameter expression="true"
-	 */
-	private boolean includeId;
+    /**
+     * Include the message identifier.
+     * 
+     * @parameter expression="true"
+     */
+    private boolean includeId;
 
-	/**
-	 * The message id offset.
-	 * 
-	 * @parameter expression="1"
-	 */
-	private int messageIdOffset;
+    /**
+     * The message id offset.
+     * 
+     * @parameter expression="1"
+     */
+    private int messageIdOffset;
 
-	/**
-	 * The message id prefix.
-	 * 
-	 * @parameter expression="E"
-	 */
-	private String messageIdPrefix;
+    /**
+     * The message id prefix.
+     * 
+     * @parameter expression="E"
+     */
+    private String messageIdPrefix;
 
-	/**
-	 * The directory containing your Java sources.
-	 * 
-	 * @parameter expression="${basedir}/src/main/java"
-	 */
-	private File javaSourcesDir;
+    /**
+     * The directory containing your Java sources.
+     * 
+     * @parameter expression="${basedir}/src/main/java"
+     */
+    private File javaSourcesDir;
 
-	/**
-	 * The directory to which the catalog will be generated.
-	 * 
-	 * @parameter expression="${basedir}/target/generated-sources"
-	 */
-	private File targetDirectory;
+    /**
+     * The directory to which the catalog will be generated.
+     * 
+     * @parameter expression="${basedir}/target/generated-sources"
+     */
+    private File targetDirectory;
 
-	/**
-	 * @parameter expression="${project}"
-	 */
-	private MavenProject project;
+    /**
+     * @parameter expression="${project}"
+     */
+    private MavenProject project;
 
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		List loggers = null;
-		try {
-			loggers = new BlammoParser(messageIdPrefix, messageIdOffset)
-					.parse(javaSourcesDir);
-		} catch (BlammoParserException bpe) {
-			throw new MojoExecutionException(bpe.getMessage() + ": line "
-					+ bpe.getLineNumber() + " in "
-					+ bpe.getSourceFile().getFile());
-		}
-		produceImplementations(loggers);
-		produceResourceFiles(loggers);
-		project.addCompileSourceRoot(targetDirectory.getAbsolutePath());
-		Resource resource = new Resource();
-		resource.setDirectory(targetDirectory.getAbsolutePath());
-		ArrayList patterns = new ArrayList();
-		patterns.add("**/*.properties");
-		resource.setIncludes(patterns);
-		project.getBuild().addResource(resource);
-	}
+    /**
+     * The Class defining the static transformer function.
+     * 
+     * @parameter
+     * @see #renderFunction
+     */
+    private String renderClass;
 
-	public void produceImplementations(List loggers)
-			throws MojoExecutionException {
-		ClassLoader loader = this.getClass().getClassLoader();
-		InputStream in = loader.getResourceAsStream("loggers.stg");
-		Reader reader = new InputStreamReader(in);
-		StringTemplateGroup group = new StringTemplateGroup(reader);
-		StringTemplate template = group.getInstanceOf("logger");
-		Iterator iterator = loggers.iterator();
-		while (iterator.hasNext()) {
-			Logger logger = (Logger) iterator.next();
-			File out = new File(targetDirectory, logger
-					.getImplementationFileName());
-			template.setAttribute("logger", logger);
-			template.setAttribute("includeId", new Boolean(includeId));
-			try {
-				out.getParentFile().mkdirs();
-				FileUtils.writeStringToFile(out, template.toString(), null);
-			} catch (IOException ioe) {
-				throw new MojoExecutionException(
-						"Failed to write implementation file.", ioe);
-			}
-			template.reset();
-		}
-	}
+    /**
+     * The transformer function, that accepts an object and renders a String.
+     * 
+     * @parameter
+     * @see #renderClass
+     */
+    private String renderFunction;
 
-	public void produceResourceFiles(List loggers)
-			throws MojoExecutionException {
-		ClassLoader loader = this.getClass().getClassLoader();
-		InputStream in = loader.getResourceAsStream("resourceBundle.stg");
-		Reader reader = new InputStreamReader(in);
-		StringTemplateGroup group = new StringTemplateGroup(reader);
-		StringTemplate template = group.getInstanceOf("logger");
-		Iterator iterator = loggers.iterator();
-		while (iterator.hasNext()) {
-			Logger logger = (Logger) iterator.next();
-			File out = new File(targetDirectory, logger.getResourceFileName());
-			template.setAttribute("logger", logger);
-			try {
-				out.getParentFile().mkdirs();
-				FileUtils.writeStringToFile(out, template.toString(), null);
-			} catch (IOException ioe) {
-				throw new MojoExecutionException(
-						"Failed to write implementation file.", ioe);
-			}
-			template.reset();
-		}
-	}
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        List loggers = null;
+        try {
+            loggers = new BlammoParser(messageIdPrefix, messageIdOffset)
+                    .parse(javaSourcesDir);
+        } catch (BlammoParserException bpe) {
+            throw new MojoExecutionException(bpe.getMessage() + ": line "
+                    + bpe.getLineNumber() + " in "
+                    + bpe.getSourceFile().getFile());
+        }
+        produceImplementations(loggers);
+        produceResourceFiles(loggers);
+        project.addCompileSourceRoot(targetDirectory.getAbsolutePath());
+        Resource resource = new Resource();
+        resource.setDirectory(targetDirectory.getAbsolutePath());
+        ArrayList patterns = new ArrayList();
+        patterns.add("**/*.properties");
+        resource.setIncludes(patterns);
+        project.getBuild().addResource(resource);
+    }
+
+    public void produceImplementations(List loggers)
+            throws MojoExecutionException {
+        ClassLoader loader = this.getClass().getClassLoader();
+        InputStream in = loader.getResourceAsStream("loggers.stg");
+        Reader reader = new InputStreamReader(in);
+        StringTemplateGroup group = new StringTemplateGroup(reader);
+        StringTemplate template = group.getInstanceOf("logger");
+        Iterator iterator = loggers.iterator();
+        while (iterator.hasNext()) {
+            Logger logger = (Logger) iterator.next();
+            File out = new File(targetDirectory, logger
+                    .getImplementationFileName());
+            template.setAttribute("logger", logger);
+            template.setAttribute("includeId", new Boolean(includeId));
+            template.setAttribute("renderClass", renderClass);
+            template.setAttribute("renderFunction", renderFunction);
+            try {
+                out.getParentFile().mkdirs();
+                FileUtils.writeStringToFile(out, template.toString(), null);
+            } catch (IOException ioe) {
+                throw new MojoExecutionException(
+                        "Failed to write implementation file.", ioe);
+            }
+            template.reset();
+        }
+    }
+
+    public void produceResourceFiles(List loggers)
+            throws MojoExecutionException {
+        ClassLoader loader = this.getClass().getClassLoader();
+        InputStream in = loader.getResourceAsStream("resourceBundle.stg");
+        Reader reader = new InputStreamReader(in);
+        StringTemplateGroup group = new StringTemplateGroup(reader);
+        StringTemplate template = group.getInstanceOf("logger");
+        Iterator iterator = loggers.iterator();
+        while (iterator.hasNext()) {
+            Logger logger = (Logger) iterator.next();
+            File out = new File(targetDirectory, logger.getResourceFileName());
+            template.setAttribute("logger", logger);
+            try {
+                out.getParentFile().mkdirs();
+                FileUtils.writeStringToFile(out, template.toString(), null);
+            } catch (IOException ioe) {
+                throw new MojoExecutionException(
+                        "Failed to write implementation file.", ioe);
+            }
+            template.reset();
+        }
+    }
 
 }
---
+++ blammo-maven-plugin/src/main/java/com/agilejava/blammo/mojo/Transformer.java
@@ -0,0 +1,25 @@
+package com.agilejava.blammo.mojo;
+
+public class Transformer {
+
+    private String className;
+    
+    private String transformation;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getTransformation() {
+        return transformation;
+    }
+
+    public void setTransformation(String transformation) {
+        this.transformation = transformation;
+    }
+    
+}
