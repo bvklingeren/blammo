7bd938d30b24f34b5faf82369749118922bd20a6
springerw <springerw>
2007-09-27 06:44:30 +0000

    Removed obsolete import.

--- blammo-adapters/blammo-commons-adapter/src/main/java/com/agilejava/blammo/adapter/commons/CommonsLoggingKitAdapter.java
+++ blammo-adapters/blammo-commons-adapter/src/main/java/com/agilejava/blammo/adapter/commons/CommonsLoggingKitAdapter.java
@@ -1,7 +1,6 @@
 package com.agilejava.blammo.adapter.commons;
 
 import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 import com.agilejava.blammo.LoggingKitAdapter;
 import com.agilejava.blammo.MessageProducer;
